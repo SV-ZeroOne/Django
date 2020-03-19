c3f82828a0792295d0e4bff0b2a4e03bab44b92f
Steve Velcev <steve@elitehost.co.za>
2020-03-19 20:10:58 +0200

    add jobs model and configure static media root

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# https://docs.djangoproject.com/en/2.2/ref/models/fields/
+# Allows us to have the model into the database
+
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
